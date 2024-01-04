4c307270c73afe255c33f692710db9c9fd9497e6
TaherJsStack <t.taher.php@gmail.com>
2024-01-04 23:10:03 +0200

    add select to select cites && range values labels

--- src/app/pages/discover/filter/filter.component.ts
+++ src/app/pages/discover/filter/filter.component.ts
@@ -7,12 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterComponent implements OnInit {
 
+  rangeUpper: number = 80;
+  rangeLower: number = 20;
+
   constructor() { }
 
   ngOnInit() {}
 
   onIonChange(e: any){
-    // console.log('onIonChange -->', e)
+    // console.log('onIonChange upper -->', e.detail.value.upper)
+    // console.log('onIonChange lower -->', e.detail.value.lower)
+    this.rangeUpper = e.detail.value.upper;
+    this.rangeLower = e.detail.value.lower;
   }
 
 }
